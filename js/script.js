844d8309d88cbba5d8fb4e2ffbc194368305d9be
quentin mccarthy <845868401@bun2-206-14.local>
2018-04-13 11:53:43 +1200

    Nav Slider - Layout done, scripting and nested layout in progress.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
 	// Slide Nav
-	$(".navButtonOpenContainer").click(function(){
+	//$(".navButtonOpenContainer").click(function(){
 		$(".navSlider").addClass("navSlider--open");
 		$(".navFadeOut").addClass("navFadeOut--active");
-	});
+	//});
 	$(".navButtonCloseContainer").click(function(){
 		$(".navSlider").removeClass("navSlider--open");
 		$(".navFadeOut").removeClass("navFadeOut--active");
+	});
+
+	// Nested Navigation
+	$(".navSliderList__listItem--withNest").click(function(){
+		$(this).toggleClass("navSliderList__listItem--openNest");
+		$(this).find(".navSliderNestedList").toggleClass("navSliderNestedList--open");
+		$(this).find(".navSliderList__caret").toggleClass("navSliderList__caret--active");
 	});
 });
