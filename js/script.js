363f725e1509d416448dcaa03154a4304be40625
quentin mccarthy <845868401@bun2-206-14.local>
2018-04-13 10:37:20 +1200

    Mobile header finished, nav slider working but still in progress

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-	$(".navButtonContainer").click(function(){
-		
+	// Slide Nav
+	$(".navButtonOpenContainer").click(function(){
+		$(".navSlider").addClass("navSlider--open");
+		$(".navFadeOut").addClass("navFadeOut--active");
+	});
+	$(".navButtonCloseContainer").click(function(){
+		$(".navSlider").removeClass("navSlider--open");
+		$(".navFadeOut").removeClass("navFadeOut--active");
 	});
 });
