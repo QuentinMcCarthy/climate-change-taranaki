e32ce1365ebd42cc520ff926bb1af7bf60b4de1e
quentin mccarthy <845868401@bun2-206-14.local>
2018-04-16 10:13:08 +1200

    Navigation Slider layout done. Scripting done. Begin on main page layout.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	// Slide Nav
-	//$(".navButtonOpenContainer").click(function(){
+	$(".navButtonOpenContainer").click(function(){
 		$(".navSlider").addClass("navSlider--open");
 		$(".navFadeOut").addClass("navFadeOut--active");
-	//});
+	});
 	$(".navButtonCloseContainer").click(function(){
 		$(".navSlider").removeClass("navSlider--open");
 		$(".navFadeOut").removeClass("navFadeOut--active");
@@ -13,6 +13,7 @@ $(document).ready(function(){
 	$(".navSliderList__listItem--withNest").click(function(){
 		$(this).toggleClass("navSliderList__listItem--openNest");
 		$(this).find(".navSliderNestedList").toggleClass("navSliderNestedList--open");
-		$(this).find(".navSliderList__caret").toggleClass("navSliderList__caret--active");
+		$(this).find(".navSliderList__anchorText").toggleClass("navSliderList__anchorText--withClosedCaret");
+		$(this).find(".navSliderList__anchorText").toggleClass("navSliderList__anchorText--withOpenCaret");
 	});
 });
