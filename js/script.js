dc3671682faa73f3acd58cb97ba98959d0f79f28
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-12-11 09:52:33 +1300

    Code overhaul; enforcing DRY/DIE, added SASS nesting

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
 	// Slide Nav
-	$(".navButtonOpenContainer").click(function(){
+	$("div[data-nav='open']").click(function(){
 		$(".navSlider").addClass("navSlider--open");
 		$(".navFadeOut").addClass("navFadeOut--active");
 	});
-	$(".navButtonCloseContainer").click(function(){
+	$("div[data-nav='close']").click(function(){
 		$(".navSlider").removeClass("navSlider--open");
 		$(".navFadeOut").removeClass("navFadeOut--active");
 	});
 
 	// Nested Navigation
-	$(".navSliderList__listItem--withNest").click(function(){
-		$(this).toggleClass("navSliderList__listItem--openNest");
-		$(this).find(".navSliderNestedList").toggleClass("navSliderNestedList--open");
-		$(this).find(".navSliderList__anchorText").toggleClass("navSliderList__anchorText--withClosedCaret");
-		$(this).find(".navSliderList__anchorText").toggleClass("navSliderList__anchorText--withOpenCaret");
+	$("li[data-nest='true']").click(function(){
+		$(this)
+			.toggleClass("navSliderList__listItem--openNest")
+			.find(".navSliderNestedList")
+				.toggleClass("navSliderNestedList--open")
+				.end()
+			.find(".navSliderList__anchorText")
+				.toggleClass("navSliderList__anchorText--withClosedCaret")
+				.end()
+			.find(".navSliderList__anchorText")
+				.toggleClass("navSliderList__anchorText--withOpenCaret");
 	});
 });
